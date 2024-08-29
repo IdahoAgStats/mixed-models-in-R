58c9054c5f2903082dcbeb8e63b029a849dbaad5
jpiaskowski <julia.piask@gmail.com>
2024-08-29 16:28:31 -0700

    set plotting conditions;

--- settings.r
+++ settings.r
@@ -1,11 +1,12 @@
 
 # code from https://www.r-causal.org/
 
+
 options(
   # set default colors in ggplot2 to colorblind-friendly
   # Okabe-Ito and Viridis palettes
-  ggplot2.discrete.colour = ggokabeito::palette_okabe_ito(),
-  ggplot2.discrete.fill = ggokabeito::palette_okabe_ito(),
+  ggplot2.discrete.colour = harrypotter::hp(10, option = "Always"),
+  ggplot2.discrete.fill = harrypotter::hp(10, option = "Always"),
   ggplot2.continuous.colour = "viridis",
   ggplot2.continuous.fill = "viridis",
   # set theme font and size
