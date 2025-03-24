7ddcf7d76ae7c5b71202a78effad75a30a20a001
Harpreet Kaur <95728406+Sidhuharp97@users.noreply.github.com>
2025-03-24 15:02:57 -0700

    changed to original

--- settings.r
+++ settings.r
@@ -6,8 +6,10 @@ options(
   # set default colors in ggplot2 to colorblind-friendly
   # Okabe-Ito and Viridis palettes
   install.packages("ggokabeito"),
-  ggplot2.discrete.colour = ggokabeito::palette_okabe_ito(aesthetics = "colour", order = 1:9),
-  ggplot2.discrete.fill = ggokabeito::palette_okabe_ito(aesthetics = "colour", order = 1:9),
+  #ggplot2.discrete.colour = ggokabeito::palette_okabe_ito(aesthetics = "colour", order = 1:9),
+  #ggplot2.discrete.fill = ggokabeito::palette_okabe_ito(aesthetics = "colour", order = 1:9),
+  ggplot2.discrete.colour = harrypotter::hp(10, option = "Always"),
+  ggplot2.discrete.fill = harrypotter::hp(10, option = "Always"),
   ggplot2.continuous.colour = "viridis",
   ggplot2.continuous.fill = "viridis",
   # set theme font and size
